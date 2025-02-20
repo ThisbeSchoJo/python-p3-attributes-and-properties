8f8468434cc7a11f965a52f65b7e1d49a6326941
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-20 10:13:07 -0500

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,37 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    # define a name property
+    # __init__ should recieve default argument for name
+    def __init__(self, name="Thisbe", breed="Corgi"):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        # name must be str
+        # name must be between 1 and 25 characters
+        if (isinstance(name, str)) and (1 <= len(name) <= 25):
+            self._name = name
+        # if name is invalid, setter method should print "Name must be string between 1 and 25 characters."
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self, breed):
+        if (breed in APPROVED_BREEDS):
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+        
+    breed = property(get_breed, set_breed)
+    # define a breed property
+    # __init__ method should receive a default argument for breed
+        # if the breed is invalid, the setter method should print:
+        # "Breed must be in list of approved breeds."
